4622fc573f46d16479df4274c6830917daa420bc
anastasia lukavsky <stacylukavsky@gmail.com>
2024-05-20 12:52:44 -0400

    add navigation to dispaly all authors

--- public/react/components/App.jsx
+++ public/react/components/App.jsx
@@ -2,46 +2,79 @@ import React, { useState, useEffect, useCallback } from 'react';
 import { PagesList } from './PagesList';
 import apiURL from '../api';
 import ArticleForm from './ArticleForm';
+import Navbar from './Navbar';
+import AuthorsList from './AuthorsList';
 
 export const App = () => {
   const [pages, setPages] = useState([]);
-  const [isAddingArticle, setIsAdingArticle] = useState(false);
+  const [isAddingArticle, setIsAddingArticle] = useState(false);
+  const [currentPage, setCurrentPage] = useState(null);
+  const [isMainVisible, setIsMainVisible] = useState(true);
 
- const fetchPages = useCallback(async () => {
-   try {
-     const response = await fetch(`${apiURL}/wiki`);
-     if (!response.ok) {
-       throw new Error('Failed to fetch articles');
-     }
-     const pagesData = await response.json();
-     setPages(pagesData);
-   } catch (err) {
-     console.log('Oh no an error! ', err);
-   }
- }, []);
+  const fetchPages = async () => {
+    try {
+      const response = await fetch(`${apiURL}/wiki`);
+      if (!response.ok) {
+        throw new Error('Failed to fetch articles');
+      }
+      const pagesData = await response.json();
+      setPages(pagesData);
+      // setIsMainVisible(true)
+    } catch (error) {
+      console.error('Error fetching pages:', error);
+    }
+  };
 
   useEffect(() => {
     fetchPages();
-  }, [fetchPages]);
+  }, []);
 
   const handleClick = () => {
-    setIsAdingArticle(true);
-    // console.log({ isAddingArticle });
+    setIsAddingArticle(true);
+  };
+
+  const handleFormSubmit = () => {
+    setIsAddingArticle(false);
+    fetchPages();
+  };
+
+  const handleNavigation = (destination) => {
+    setCurrentPage(destination);
+    setIsMainVisible(false)
+    setIsAddingArticle(false);
+  };
+
+  const renderPage = () => {
+    switch (currentPage) {
+      case 'authors':
+        return <AuthorsList setIsMainVisible={setIsMainVisible}/>;
+      default:
+        return <h1>Welcome to WikiVerse!</h1>;
+    }
   };
 
   return (
     <main>
-      {isAddingArticle ? (
-        <ArticleForm pages={pages} setPages={setPages} fetchPages={fetchPages}/>
+      <div>
+        <Navbar handleNavigation={handleNavigation}/>
+        {renderPage()}
+      </div>
+
+      {isAddingArticle  ? (
+        <ArticleForm onSubmit={handleFormSubmit} />
       ) : (
         <>
-          <h1>WikiVerse</h1>
-          <h2>An interesting 📚</h2>
-          <PagesList pages={pages} fetchPages={fetchPages}/>
+        {isMainVisible &&
+          <PagesList
+          pages={pages}
+          fetchPages={fetchPages}
+          setIsAddingArticle={setIsAddingArticle}
+          />
+        }
           <br />
           <button onClick={handleClick}>Add an Article</button>
-        </>
-      )}
+          </>
+          )}
     </main>
   );
 };
